7ae7ab00d5fe0d9d4fabf15ec21470b22ee9ce12
thalha-dev <dev.thalha@gmail.com>
2023-05-14 21:42:09 +0530

    prepare branch for next project

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,9 @@
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import Card from "./components/Card";
 import "./App.css";
-import data from "../data";
 
 const App = () => {
-  const cards = data.map((item) => {
-    return <Card key={item.id} item={item} />;
-  });
-
   return (
     <div>
-      <Navbar />
-      <Hero />
-      <section className="cards">{cards}</section>
+      <h2>MEME</h2>
     </div>
   );
 };
--- src/components/Navbar.jsx
+++
@@ -1,9 +0,0 @@
-const Navbar = () => {
-  return (
-    <nav>
-      <img src="/airbnb-logo.png" alt="logo" />
-    </nav>
-  );
-};
-
-export default Navbar;
